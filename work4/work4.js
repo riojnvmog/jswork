4ba7ce46e06eec35df70d9330696d1577fef57b2
riojnvmog <2927404489@qq.com>
2019-10-13 22:52:04 +0800

    jswork

--- work4/work4.js
+++ work4/work4.js
@@ -16,3 +16,13 @@ for (let i=1;i<=level;++i){
     }
     document.write("<br>")
 }
+let str ='<table border="9">'
+for (let i=1;i<10;++i){
+    str +='<tr>'
+    for(var j=1;j<=i;++j){
+        str += '<td>' +j +'*' +i + '=' + (j*i) +'</td>'
+    }
+    str +='</tr>'
+}
+str +='</table>'
+document.getElementById('table').innerHTML =str
