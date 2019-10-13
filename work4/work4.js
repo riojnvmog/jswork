b15e813fbbeadd34bcc415926f905a3563a59456
riojnvmog <2927404489@qq.com>
2019-10-13 20:21:22 +0800

    wrok1

---
+++ work4/work4.js
@@ -0,0 +1,18 @@
+document.write("<p>金字塔</p>")
+let level= prompt('请设置金字塔的层数')
+level=parseFloat(level)&&Number(level)
+if (isNaN(level)){
+    alert ('金字塔的层数是数字')
+}
+for (let i=1;i<=level;++i){
+    let blank = level - i;
+    for(let k=0;k<blank;++k){ 
+        document.write('&nbsp;');
+    }
+    let star =i*2-1
+    for(let j=0;j<star;++j){
+        document.write('*')
+
+    }
+    document.write("<br>")
+}
