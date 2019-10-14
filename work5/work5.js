b9819182b37d0aaf78e6ba5b635902ae5ec2279f
riojnvmog <2927404489@qq.com>
2019-10-14 20:56:29 +0800

    work5

--- work5/work5.js
+++ work5/work5.js
@@ -17,3 +17,14 @@ for(let i=9; i>0; --i){
 }str +='</tr>'}
 str +='</table>'
 document.getElementById('table1').innerHTML =str 
+
+str ='<table border ="1">'
+for (let i=9;i>0;--i){
+    str +='<tr>'
+    for(var j=i;j>0;--j){
+    str +='<td>&nbsp</td>'
+}
+str +='</tr>'
+}
+str +='</table>'
+document.getElementById('table3').innerHTML =str
