a4ad6a3428b188ff25654c2cd9b94dc4589725ef
riojnvmog <2927404489@qq.com>
2019-10-15 22:50:52 +0800

    work6

---
+++ work6/pai.js
@@ -0,0 +1,15 @@
+function calculate(){
+    let num = document.getElementById(''.value)
+    num = parseInt(num)&&Number(num)
+    if (!isNaN(num)){
+        document.getElementById('').value = pi()
+    }
+}
+
+function factorial(n){
+    let sum =1
+    for(let i =1   ){
+
+    }
+    
+}
